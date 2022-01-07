2d60be8a54db7023bb8418d858d827a163ed2200
Leonardo Biazom <lbiazom@gmail.com>
2022-01-07 19:13:11 -0300

    Incremento do NLP
    Editado NLU, Forms, Utters e Action para o bot responder automaticamente no início de sessão

--- actions/actions.py
+++ actions/actions.py
@@ -2,7 +2,8 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SlotSet
+from rasa_sdk.events import SlotSet, SessionStarted, ActionExecuted
+
 
 
 class ActionHelloWorld(Action):
@@ -37,3 +38,25 @@ class ActionSetActivictyDetailsPreference(Action):
             print('deny')
             return [SlotSet("address_number", -1), SlotSet("address_street", 'Desconhecido'), SlotSet("address_district", 'Desconhecido'), SlotSet("address_number", 'Desconhecido')]
         return []
+
+class ActionSessionStart(Action):
+    def name(self) -> Text:
+        return "action_session_start"
+
+    async def run(
+      self, dispatcher, tracker: Tracker, domain: Dict[Text, Any]
+    ) -> List[Dict[Text, Any]]:
+
+        # the session should begin with a session_started event
+        events = [SessionStarted()]
+
+        # any slots that should be carried over should come after the
+        # session_started event
+        #events.extend(self.fetch_slots(tracker))
+
+        # an action_listen should be added at the end as a user message follows
+        dispatcher.utter_message(response="utter_greet")
+        events.append(ActionExecuted("action_listen"))
+
+
+        return events
