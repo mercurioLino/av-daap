b0da97c5baf52e125c32a394cb3f5fd8bb6ebea5
Vinicius Marchi <viniciuspmarchi@gmail.com>
2022-01-11 15:07:41 -0400

    Actions para setar o valor dos slots booleanos

--- actions/actions.py
+++ actions/actions.py
@@ -20,24 +20,6 @@ class ActionHelloWorld(Action):
         return []
 
 
-'''
-Action que define o valor dos slots de endereço.
-'''
-class ActionSetActivictyDetailsPreference(Action):
-    def name(self):
-        return "action_set_address_details_preference"
-
-    def run(self, dispatcher, tracker, domain):
-
-        intent = tracker.latest_message["intent"].get("name")
-
-        if intent == "affirm" or intent == "define_address_detail":
-            print('affirm')
-            return [SlotSet("address_landmark", None)]
-        elif intent == "deny":
-            print('deny')
-            return [SlotSet("address_number", -1), SlotSet("address_street", 'Desconhecido'), SlotSet("address_district", 'Desconhecido'), SlotSet("address_number", 'Desconhecido')]
-        return []
 
 class ActionSessionStart(Action):
     def name(self) -> Text:
@@ -60,3 +42,98 @@ class ActionSessionStart(Action):
 
 
         return events
+
+
+
+
+
+# --------------- Actions que setam os valores dos Slots booleanos -----------------------------
+# slot animal_urgency
+class ActionSetAnimalUrgency(Action):
+    def name(self):
+        return "action_set_animal_urgency"
+
+    def run(self, dispatcher, tracker, domain):
+
+        intent = tracker.latest_message["intent"].get("name")
+
+        if intent == "affirmation" or intent == "wanna_animal_urgency":
+            print('affirmation')
+            return [SlotSet("animal_urgency", True)]
+        elif intent == "negation":
+            print('negation')
+            return [SlotSet("animal_urgency", False)]
+        return []
+
+
+
+# slot medical_attention
+class ActionSetAnimalMedicalAttention(Action):
+    def name(self):
+        return "action_set_medical_attention"
+
+    def run(self, dispatcher, tracker, domain):
+
+        intent = tracker.latest_message["intent"].get("name")
+
+        if intent == "affirmation":
+            print('affirmation')
+            return [SlotSet("medical_attention", True)]
+        elif intent == "negation":
+            print('negation')
+            return [SlotSet("medical_attention", False)]
+        return []
+
+
+# slot private_property
+class ActionSetPrivateProperty(Action):
+    def name(self):
+        return "action_set_private_property"
+
+    def run(self, dispatcher, tracker, domain):
+
+        intent = tracker.latest_message["intent"].get("name")
+
+        if intent == "affirmation":
+            print('affirmation')
+            return [SlotSet("private_property", True)]
+        elif intent == "negation":
+            print('negation')
+            return [SlotSet("private_property", False)]
+        return []
+
+# slot maus_tratos
+class ActionSetTratos(Action):
+    def name(self):
+        return "action_set_maus_tratos"
+
+    def run(self, dispatcher, tracker, domain):
+
+        intent = tracker.latest_message["intent"].get("name")
+
+        if intent == "affirmation":
+            print('affirmation')
+            return [SlotSet("maus_tratos", True)]
+        elif intent == "negation":
+            print('negation')
+            return [SlotSet("maus_tratos", False)]
+        return []
+
+'''
+Action que define o valor dos slots de endereço.
+'''
+class ActionSetActivictyDetailsPreference(Action):
+    def name(self):
+        return "action_set_address_details_preference"
+
+    def run(self, dispatcher, tracker, domain):
+
+        intent = tracker.latest_message["intent"].get("name")
+
+        if intent == "affirm" or intent == "define_address_detail":
+            print('affirm')
+            return [SlotSet("address_landmark", None)]
+        elif intent == "deny":
+            print('deny')
+            return [SlotSet("address_number", -1), SlotSet("address_street", 'Desconhecido'), SlotSet("address_district", 'Desconhecido'), SlotSet("address_number", 'Desconhecido')]
+        return []
