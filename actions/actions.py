79a924b66c045253c261b6a1d90e3afc987a1b50
mercurioLino <leonardomercurio@hotmail.com>
2022-01-11 16:49:50 -0300

    Definindo action de envio de emails

--- actions/actions.py
+++ actions/actions.py
@@ -1,25 +1,14 @@
 from typing import Any, Text, Dict, List
 
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet, SessionStarted, ActionExecuted
-
-
-
-class ActionHelloWorld(Action):
-
-    def name(self) -> Text:
-        return "action_hello_world"
-
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_message(text="Hello World!")
-
-        return []
-
-
 
 class ActionSessionStart(Action):
     def name(self) -> Text:
@@ -137,3 +126,47 @@ class ActionSetActivictyDetailsPreference(Action):
             print('deny')
             return [SlotSet("address_number", -1), SlotSet("address_street", 'Desconhecido'), SlotSet("address_district", 'Desconhecido'), SlotSet("address_number", 'Desconhecido')]
         return []
+
+# --------------- Action que envia email -----------------------------
+class ActionEmailSender(Action):
+    def name(self):
+        return "action_email_sender"
+
+    def run(self, dispatcher, tracker, domain):
+        #Start do server
+        host = "smtp.gmail.com"
+        port = "587"
+        login = "epythonteste@gmail.com"
+        senha = "Compass123456"
+
+        server = smtplib.SMTP(host, port)
+        
+        server.ehlo()
+        server.starttls()
+
+        server.login(login, senha)
+
+        #Construção do corpo do email
+        ##Texto com link
+        corpo = ''
+        email_msg = MIMEMultipart()
+        email_msg['From'] = login
+        email_msg['To'] = ''
+        email_msg['Subject'] = ""
+        email_msg.attach(MIMEText(corpo, 'html'))
+
+        ##Anexo
+        caminho_anexo = '' ##Inserir aqui o caminho do arquivo
+        attachment = open(caminho_anexo, 'rb')
+        att = MIMEBase('application', 'octet-stream')
+        att.set_payload(attachment.read())
+        encoders.encode_base64(att)
+        att.add_header('Content-Disposition', f'attachment; filename=') #Inserir no filename o nome do arquivo
+        attachment.close()
+        email_msg.attach(att)
+
+        #Envio do email
+        server.sendmail(email_msg['From'], email_msg['To'], email_msg.as_string())
+
+        #Fechando o servidor
+        server.quit()
