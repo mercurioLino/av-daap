d96f10f842c9b30c8b1a8e7f8e7a010f43df819a
Dezan <36449909+NathanDezan@users.noreply.github.com>
2022-01-19 01:21:03 -0400

    Pipeline para classificação de imagens

---
+++ actions/transform.py
@@ -0,0 +1,43 @@
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+from skimage.feature import hog
+from skimage.io import imread
+from skimage.transform import rescale
+import skimage 
+
+class RGB2GrayTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+ 
+    def fit(self, X, y=None):
+        return self
+ 
+    def transform(self, X, y=None):
+        return np.array([skimage.color.rgb2gray(img) for img in X])
+     
+class HogTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, y=None, orientations=9,
+                 pixels_per_cell=(8, 8),
+                 cells_per_block=(3, 3), block_norm='L2-Hys'):
+        self.y = y
+        self.orientations = orientations
+        self.pixels_per_cell = pixels_per_cell
+        self.cells_per_block = cells_per_block
+        self.block_norm = block_norm
+ 
+    def fit(self, X, y=None):
+        return self
+ 
+    def transform(self, X, y=None):
+ 
+        def local_hog(X):
+            return hog(X,
+                       orientations=self.orientations,
+                       pixels_per_cell=self.pixels_per_cell,
+                       cells_per_block=self.cells_per_block,
+                       block_norm=self.block_norm)
+ 
+        try:
+            return np.array([local_hog(img) for img in X])
+        except:
+            return np.array([local_hog(img) for img in X])
---
+++ actions/utils.py
@@ -0,0 +1,22 @@
+def fix_shape(data):
+    count = 0
+    aux = dict()
+    aux['data'] = []
+    aux['label'] = []
+
+    for i in data['data']:
+        if count == 445:
+            print(f'fix block {count}')
+        else:
+            aux['data'].append(i)
+        count += 1
+    
+    count = 0
+    for i in data['label']:
+        if count == 445:
+            print(f'fix block {count}')
+        else:
+            aux['label'].append(i)
+        count += 1
+
+    return aux
