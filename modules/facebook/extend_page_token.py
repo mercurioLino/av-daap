5ba8e45aeed8ca1875b21cf06f99c9ee4b7b1451
Vinicius Marchi <viniciuspmarchi@gmail.com>
2022-01-25 13:23:55 -0400

    Removendo token

--- modules/facebook/extend_page_token.py
+++ modules/facebook/extend_page_token.py
@@ -8,9 +8,9 @@ Script que aumenta o tempo de duração do token de acesso a página. Por padrã
 dura apenas 2 horas, logo precisa ser extendido, obtendo assim um tempo de duração 
 de 2 meses e evitando problemas de autenticação.
 """
-graph = fb.GraphAPI('EAAJuk0ZAYiKwBAOReBPmZBGABLLrFzHDEqUvQSj1OAAUfn8vx1TTX3MaYmohVsHhwUjBxIKO8vdromZCWyP8J5QQGZBnhrvWYOzZAKwnPZA1TToPVconh6KBk9xepxIeuaBEkmxvHpIXPMU2EHKOcsYgpATuoZCWTh2defPTU4Xls2HWynrxJCrvIafsamFCpJv67kSYW99FswZAygygVCnA')
-app_id = '684528772876460'
-app_secret = '3c7530511daa730d13b039df31066d4f'
+graph = fb.GraphAPI('')
+app_id = ''
+app_secret = ''
 
 # Extende o tempo de expiração do token
 extended_token = graph.extend_access_token(app_id, app_secret)
